7b5e0fbac0bcb387e17ea9650c3eff3555a8e0f8
hidayatfariz <hidayatullahfarizqi03@gmail.com>
2023-06-22 15:21:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exportfigma/pembayaran_activity.java
@@ -0,0 +1,109 @@
+package com.example.exportfigma;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		pembayaran
+	 *	@date 		Thursday 22nd of June 2023 04:16:16 AM
+	 *	@title 		Page 2
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class pembayaran_activity extends Activity {
+
+	
+	private View _bg__pembayaran_ek2;
+	private View rectangle_1;
+	private ImageView fill_173;
+	private TextView pembayaran_ek3;
+	private TextView pesanan;
+	private TextView asrama_itb_jatinangor;
+	private TextView alamat_pengiriman;
+	private View rectangle_5;
+	private View rectangle_6;
+	private ImageView fill_278;
+	private TextView isi_detail_alamat;
+	private TextView ganti_alamat;
+	private View rectangle_4;
+	private ImageView fill_202;
+	private TextView _23_000;
+	private TextView _25_000;
+	private TextView mie_ayam_bakso_cincang;
+	private View line_3;
+	private ImageView image_1;
+	private View ellipse_1;
+	private View ellipse_2;
+	private TextView __;
+	private TextView _1__;
+	private View rectangle_7;
+	private View rectangle_8;
+	private View line_3_ek1;
+	private TextView _25_000_23_000_12_000_3_000_38_000;
+	private TextView harga_ongkir_biaya_layanan_total_pembayaran;
+	private TextView ringkasan_pembayaran;
+	private View line_4;
+	private View rectangle_6_ek1;
+	private TextView pesan_dan_antar_sekarang;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pembayaran);
+
+		
+		_bg__pembayaran_ek2 = (View) findViewById(R.id._bg__pembayaran_ek2);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		fill_173 = (ImageView) findViewById(R.id.fill_173);
+		pembayaran_ek3 = (TextView) findViewById(R.id.pembayaran_ek3);
+		pesanan = (TextView) findViewById(R.id.pesanan);
+		asrama_itb_jatinangor = (TextView) findViewById(R.id.asrama_itb_jatinangor);
+		alamat_pengiriman = (TextView) findViewById(R.id.alamat_pengiriman);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		fill_278 = (ImageView) findViewById(R.id.fill_278);
+		isi_detail_alamat = (TextView) findViewById(R.id.isi_detail_alamat);
+		ganti_alamat = (TextView) findViewById(R.id.ganti_alamat);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		fill_202 = (ImageView) findViewById(R.id.fill_202);
+		_23_000 = (TextView) findViewById(R.id._23_000);
+		_25_000 = (TextView) findViewById(R.id._25_000);
+		mie_ayam_bakso_cincang = (TextView) findViewById(R.id.mie_ayam_bakso_cincang);
+		line_3 = (View) findViewById(R.id.line_3);
+		image_1 = (ImageView) findViewById(R.id.image_1);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		__ = (TextView) findViewById(R.id.__);
+		_1__ = (TextView) findViewById(R.id._1__);
+		rectangle_7 = (View) findViewById(R.id.rectangle_7);
+		rectangle_8 = (View) findViewById(R.id.rectangle_8);
+		line_3_ek1 = (View) findViewById(R.id.line_3_ek1);
+		_25_000_23_000_12_000_3_000_38_000 = (TextView) findViewById(R.id._25_000_23_000_12_000_3_000_38_000);
+		harga_ongkir_biaya_layanan_total_pembayaran = (TextView) findViewById(R.id.harga_ongkir_biaya_layanan_total_pembayaran);
+		ringkasan_pembayaran = (TextView) findViewById(R.id.ringkasan_pembayaran);
+		line_4 = (View) findViewById(R.id.line_4);
+		rectangle_6_ek1 = (View) findViewById(R.id.rectangle_6_ek1);
+		pesan_dan_antar_sekarang = (TextView) findViewById(R.id.pesan_dan_antar_sekarang);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
